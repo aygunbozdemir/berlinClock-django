49dcb484e2edae0bc6fc92fd2b269d647f8baff4
aygunbozdemir <abozdemir@protel.com.tr>
2021-02-26 01:01:41 +0300

    3 pages implemented

--- berlinClock/views.py
+++ berlinClock/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 # Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, "homepage.html", context=None)
 
-def index(request):
-    return HttpResponse("<h1>Hello Word!!!<h1>")
+
+class AboutMePageView(TemplateView):
+    template_name = 'aboutme.html'
+    # def get(self, request, **kwargs):
+    # return render(request, "aboutme.html", context=None)
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+    # def get(self, request, **kwargs):
+    # return render(request, "homepage.html", context=None)
